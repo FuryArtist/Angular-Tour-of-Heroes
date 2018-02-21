d7376ed60067de52596e692decbc420d3d856e37
sladkoewka <sladkoewka12@gmail.com>
2018-02-21 14:05:31 +0400

    Services

---
+++ my-app/src/app/message.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+
+  constructor() { }
+}
